9ef4c48a2cd18fa5c65bd3e4ffea01183f94a3ab
amarpaviall <er.amarpaviall@gmail.com>
2025-06-19 19:35:16 -0400

    add twig template for post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use Amar\Framework\Controller\AbstractController;
 use Amar\Framework\Http\Response;
 
-class PostController {
+class PostController extends AbstractController
+{
 
-  public function show(int $id) : Response {
-    $content = "This is Post {$id}";
-    return new Response($content);
+  public function show(int $id): Response
+  {
+    // $content = "This is Post {$id}";
+    // return new Response($content);
+
+    return $this->render('post.html.twig', [
+      'postId' => $id
+    ]);
   }
 }
